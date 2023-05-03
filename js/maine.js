b0267ba6dffc38240375d3b3927dcd171106b9f0
T-Y-LLI-E-H-K-A <125465286+T-Y-LLI-E-H-K-A@users.noreply.github.com>
2023-05-03 21:35:03 +0700

    Update maine.js

--- js/maine.js
+++ js/maine.js
@@ -1,3 +1,13 @@
 Fancybox.bind('[data-fancybox="video"]', {
     hideScrollbar: false,
   });
+
+$(function () { 
+
+    var mixer = mixitup('.blog__list');
+
+    $('.filter-box__btn').on('click', function () {
+        $('.filter-box__btn').removeClass('filter-box__btn--active')
+        $(this).addClass('filter-box__btn--active')
+    })
+})
