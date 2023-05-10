fae136d11e857f08ec0a0ce930ab4c593c5c919d
T-Y-LLI-E-H-K-A <125465286+T-Y-LLI-E-H-K-A@users.noreply.github.com>
2023-05-10 22:35:44 +0700

    Update maine.js

--- js/maine.js
+++ js/maine.js
@@ -1,8 +1,8 @@
 Fancybox.bind('[data-fancybox="video"]', {
     hideScrollbar: false,
-  });
+});
 
-$(document).ready(function () { 
+$(document).ready(function () {
 
     var mixer = mixitup('.blog__list');
 
@@ -15,7 +15,20 @@ $(document).ready(function () {
         slidesToShow: 2,
         arrows: true,
         dots: true,
-        appendArrows:$('.switch__btns-box'),
-        appendDots:$('.switch__dots-box')
+        appendArrows: $('.switch__btns-box'),
+        appendDots: $('.switch__dots-box')
     });
+
+    $('.faq__list-link').on('click', function(e) {
+        e.preventDefault()
+        if ($(this).hasClass('faq__list-link--active')) {
+            $(this).removeClass('faq__list-link--active')
+            $(this).children('.faq-link__content-txt').slideUp()
+        } else {
+            $('.faq__list-link').removeClass('faq__list-link--active')
+            $('.faq-link__content-txt').slideUp()
+            $(this).addClass('faq__list-link--active')
+            $(this).children('.faq-link__content-txt').slideDown()
+        }
+    })
 })
